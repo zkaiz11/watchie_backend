7d9853762f3b47f4453a7454a58440d2df2fae09
Kai <hsokchamrern@gmail.com>
2023-07-03 02:33:38 +0700

    cors added

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ const PORT: number = parseInt(process.env.PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'https://watchie-xl1e.vercel.app',
+    ],
     credentials: true,
   });
   await app.listen(PORT || 3000);
